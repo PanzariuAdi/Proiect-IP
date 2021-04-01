9438f5db6c2c5e56449e611620cee606675133c5
Andrew6k <75756033+Andrew6k@users.noreply.github.com>
2021-04-01 17:41:36 +0300

    Add files via upload

---
+++ Server-client/Client.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.net.*;
+public class Client {
+    public static void main(String arg[]){
+        try {
+            Socket s = new Socket("127.0.0.1", 3000);
+            System.out.println("Connected");
+        }catch (Exception e){}
+    }
+}
---
+++ Server-client/Server.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.net.*;
+public class Server {
+    public static void main(String arg[]){
+        try {
+            ServerSocket server = new ServerSocket(3000);
+            Socket s = server.accept();
+
+            System.out.println("Connected");
+        }catch(Exception e){}
+    }
+}
