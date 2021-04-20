0c6702587d5e87817a19ea5217ecb86a1a51ac75
PanzariuAdi <panzariu.adi00@gmail.com>
2021-04-20 20:06:37 +0300

    Add files via upload

--- Algoritm/Main.java
+++ Algoritm/Main.java
@@ -3,9 +3,11 @@ package lab07;
 public class Main {
 
     public static void main(String[] args) {
-        Person p1 = new Person();
-        p1.addHeartMeasure(60);
-        p1.addHeartMeasure(100);
-        p1.addHeartMeasure(150);
+        Person p1 = new Person("5000108071347");
+        p1.addHeartMeasure(40);
+        p1 .addHeartMeasure(40);
+        p1 .addHeartMeasure(40);
+
+
     }
 }
--- Algoritm/Person.java
+++ Algoritm/Person.java
@@ -1,13 +1,17 @@
 package lab07;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Person {
     private String firstName;
     private String secondName;
     private String cnp;
     private String address;
+
     private ArrayList<Float> heartMeasures = new ArrayList<Float>();
+
+    public Person(String cnp) { this.cnp = cnp; }
 
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
@@ -34,8 +38,41 @@ public class Person {
 
     public void addHeartMeasure(float value) {
         this.heartMeasures.add(value);
+
         if (standardDeviation() > 20) {
-            System.out.println("Something IS NOT OK!");
+            System.out.println("Value too different from previous measurements !");
+        }
+
+        checkHeartRate(value);
+    }
+
+    private int getAgeInYears() {
+        String dateString = cnp.substring(1, 7);
+        int birthYear = Integer.parseInt(cnp.substring(1, 3));
+        if (cnp.charAt(0) == '5' || cnp.charAt(0) == '6') birthYear += 100;
+        Date date = new Date();
+        return date.getYear() - birthYear;
+    }
+
+    private void checkHeartRate(float value) {
+        int personAge = getAgeInYears();
+        if (personAge <= 1) {
+            if (!(value >= 80 &&  value <= 160))
+                showMessage();
+        } else if (personAge <= 2){
+            if(!(value >= 80 && value <= 130))
+                showMessage();
+        } else if (personAge <= 10) {
+            if(!(value >= 70 && value <= 110))
+                showMessage();
+        } else {
+            if (!(value >= 60 && value <= 100))
+                showMessage();
         }
     }
+
+    private void showMessage() {
+        System.out.println("Heart rate inappropriate for this age !");
+    }
+
 }
