564520d1dea86970e6e8bbd9b86b26bd9a251c60
Fisher121 <58854288+Fisher121@users.noreply.github.com>
2021-04-24 21:20:33 +0300

    Add files via upload

---
+++ clientIOT/src/main/java/client/Main.java
@@ -0,0 +1,12 @@
+package client;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+      //CCH.sendData("user", "2 March 2021 17:00","70","3000","6000","4.5","45");
+        System.out.println(CCH.getPacient("user"));
+
+
+    }
+}
